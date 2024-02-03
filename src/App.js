0533816eabd15c56ba726b779100483a6cc59d74
nathansabin <sabinnat001@gmail.com>
2024-02-03 15:42:12 -0800

    added footer with links svg and bootstrap classes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import Button from './components/button/button';
 import Home from './pages/home/home';
-
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
         <Home />
+        <Footer /> 
       </div>
     </div>
   );
