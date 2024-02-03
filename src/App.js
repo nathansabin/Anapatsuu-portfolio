a6c14da252fc52a4d724ec7cfe12156d653c38e2
nathansabin <sabinnat001@gmail.com>
2024-02-03 11:57:33 -0800

    button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Button from './components/button/button';
+
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
-        
+        <h1>Anapatsuu</h1>
+        <Button 
+        text="Tester"/>
       </div>
     </div>
   );
---
+++ src/components/button/button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './button.css';
+
+function Button(props) {
+    console.log(props);
+    return (
+        <a href={`/${props.category}`}>
+            <button className='button'>
+                <h3>
+                    {props.text}
+                </h3>
+            </button>
+        </a>
+    );
+}
+
+export default Button;
