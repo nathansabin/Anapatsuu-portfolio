f0935594967d0da368c04b57c982127426e6d6e7
nathansabin <sabinnat001@gmail.com>
2024-02-03 16:18:31 -0800

    added router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Home from './pages/home/home';
 import Footer from './components/footer/footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
-        <Home />
+        <Router>
+          <main>
+            <Routes>
+              <Route 
+              path='/'
+              element={<Home />}/>
+            </Routes>
+          </main>
+        </Router>
         <Footer /> 
       </div>
     </div>
