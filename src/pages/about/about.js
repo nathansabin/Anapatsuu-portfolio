ab44ecff418ca07cc84cf29329a6f5d573af47d2
nathansabin <sabinnat001@gmail.com>
2024-02-13 20:12:53 -0800

    style changes and correct text for about

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -9,11 +9,15 @@ function About() {
         <div className='about-container'>
             <div className='about-box'>
                 <div className='about-content'>
-                    <h2 className='about-hero'>About</h2>
+                    <h2 className='about-hero'>⊹˙⋆About⋆˙⊹</h2>
                     <hr/>
                     <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ac odio tempor orci dapibus ultrices in. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Sed euismod nisi porta lorem. At varius vel pharetra vel turpis. Et tortor consequat id porta nibh venenatis cras sed felis. Nibh cras pulvinar mattis nunc sed blandit. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Porttitor rhoncus dolor purus non. Tellus orci ac auctor augue mauris augue neque gravida. Eleifend mi in nulla posuere sollicitudin. Massa eget egestas purus viverra accumsan. Morbi tempus iaculis urna id volutpat lacus laoreet.
+                    Hi! My name is Ana, I’ve been an artist for over 15 years of experience and still ongoing. I practice different variations of art, Digital Art, Traditional Art, Animation, Storyboarding, etc. I’m currently a college student majoring in Media Arts and Motion Design (Animation). I have some of my school work presented in this portfolio and personal projects. I have some commissions I’ve worked on in the past as well.
+                    I work hard and I’m incredibly motivated in the art field, especially Animation since it’s something I’m still getting the grasp of but absolutely enjoying it! 
+                    I intend to continue pursuing Animation as my major, however I will continue to do Traditional and Digital Art as I love the form of drawing and art itself, as I’m really good with creating characters and dramatic poses.
+                    Thank you for reading about me! Hope we can get to know each other! If you have any concerns or questions don’t be afraid to reach out! 
                     </p>
+                    <em>~Ana</em>
                 </div>
             </div>
             <img className='about-img' alt='betty' src={betty} />
